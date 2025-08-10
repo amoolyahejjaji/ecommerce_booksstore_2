b4e77e1bf187ad08aba94d32a6421be5c637c8d1
amoolyahejjaji <amoolya.p@s.amity.edu>
2025-08-10 15:48:27 +0530

    c3:added array of books with title,author,price,stock

--- script.js
+++ script.js
@@ -1,1 +1,90 @@
 console.log("Books catalog loaded.");
+const books = [
+  {
+    id: 1,
+    title: "The Alchemist",
+    author: "Paulo Coelho",
+    price: 250,
+    stock: 10
+  },
+  {
+    id: 2,
+    title: "Rich Dad Poor Dad",
+    author: "Robert Kiyosaki",
+    price: 300,
+    stock: 5
+  },
+  {
+    id: 3,
+    title: "Atomic Habits",
+    author: "James Clear",
+    price: 350,
+    stock: 7
+  },
+  {
+    id: 4,
+    title: "Deep Work",
+    author: "Cal Newport",
+    price: 280,
+    stock: 6
+  },
+  {
+    id: 5,
+    title: "Ikigai",
+    author: "Héctor García",
+    price: 220,
+    stock: 2
+  }
+
+
+  {
+    id: 6,
+    title: "The outsiders",
+    author: "hinton",
+    price: 259,
+    stock: 10
+  },
+  {
+    id: 7,
+    title: "to kill the mockingbird",
+    author: "harper lee",
+    price: 3000,
+    stock: 50
+  },
+  {
+    id: 8,
+    title: "milk and  honey",
+    author: "rupi kaur",
+    price: 950,
+    stock: 17
+  },
+  {
+    id: 9,
+    title: "a man callaed ove",
+    author: "fredrik backman",
+    price: 290,
+    stock: 60
+  },
+  {
+    id: 10,
+    title: "hand maid's tale",
+    author: "margaret atwood",
+    price: 200,
+    stock: 82
+  },
+  {
+    id: 11,
+    title: "harry potter",
+    author: "rowling",
+    price: 2000,
+    stock: 2
+  },
+  {
+    id: 12,
+    title: "too good to be true",
+    author: "prajakta koli",
+    price: 2000,
+    stock: 1
+  },
+
+];
